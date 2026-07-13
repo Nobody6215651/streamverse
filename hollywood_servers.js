f03227c4cfcdd7fb1ce18cf772db8e6f0e2a80d5
Nobody6215651 <saimmirza652@gmail.com>
2026-07-13 05:31:55 -0700

    Update hollywood_servers.js

--- hollywood_servers.js
+++ hollywood_servers.js
@@ -1,29 +1,62 @@
 /**
- * StreamVerse Dedicated Hollywood Mirror Configuration Array
+ * StreamVerse Hollywood CDN Distribution Matrix
+ * Path: /js/hollywood_servers.js
  */
-const hollywoodServersList = [
-    {
-        name: "Master Server (VidSrc)",
-        generateUrl: (tmdbId, s, e, type) => {
-            return type === 'movie' 
-                ? `https://vidsrc.me/embed/movie?tmdb=${tmdbId}` 
-                : `https://vidsrc.me/embed/tv?tmdb=${tmdbId}&sea=${s}&epi=${e}`;
+(function (window) {
+    'use strict';
+
+    class HollywoodCDNCluster {
+        constructor() {
+            this.clusterRegion = "GLOBAL-WEST";
+            this.routingNodes = [
+                {
+                    id: "h-node-1",
+                    name: "Primary CDN (VidSrc.me)",
+                    priority: 10,
+                    generateUrl: (tmdbId, s, e, type) => {
+                        const secureId = encodeURIComponent(tmdbId);
+                        return type === 'movie'
+                            ? `https://vidsrc.me/embed/movie?tmdb=${secureId}`
+                            : `https://vidsrc.me/embed/tv?tmdb=${secureId}&sea=${Number(s)}&epi=${Number(e)}`;
+                    }
+                },
+                {
+                    id: "h-node-2",
+                    name: "Fallback CDN (2Embed)",
+                    priority: 8,
+                    generateUrl: (tmdbId, s, e, type) => {
+                        const secureId = encodeURIComponent(tmdbId);
+                        return type === 'movie'
+                            ? `https://embed.2embed.to/embed/tmdb/movie?id=${secureId}`
+                            : `https://embed.2embed.to/embed/tmdb/tv?id=${secureId}&s=${Number(s)}&e=${Number(e)}`;
+                    }
+                },
+                {
+                    id: "h-node-3",
+                    name: "Alternative CDN (AutoEmbed)",
+                    priority: 5,
+                    generateUrl: (tmdbId, s, e, type) => {
+                        const secureId = encodeURIComponent(tmdbId);
+                        return type === 'movie'
+                            ? `https://player.autoembed.cc/embed/movie/${secureId}`
+                            : `https://player.autoembed.cc/embed/tv/${secureId}/${Number(s)}/${Number(e)}`;
+                    }
+                }
+            ];
+            Object.freeze(this.routingNodes);
         }
-    },
-    {
-        name: "Mirror Server 2 (2Embed)",
-        generateUrl: (tmdbId, s, e, type) => {
-            return type === 'movie'
-                ? `https://embed.2embed.to/embed/tmdb/movie?id=${tmdbId}`
-                : `https://embed.2embed.to/embed/tmdb/tv?id=${tmdbId}&s=${s}&e=${e}`;
+
+        getNodes() {
+            return this.routingNodes;
         }
-    },
-    {
-        name: "Mirror Server 3 (AutoEmbed)",
-        generateUrl: (tmdbId, s, e, type) => {
-            return type === 'movie'
-                ? `https://player.autoembed.cc/embed/movie/${tmdbId}`
-                : `https://player.autoembed.cc/embed/tv/${tmdbId}/${s}/${e}`;
+
+        getHighestPriorityNode() {
+            return this.routingNodes[0];
         }
     }
-];
+
+    Object.freeze(HollywoodCDNCluster);
+    Object.freeze(HollywoodCDNCluster.prototype);
+    window.HollywoodServers = new HollywoodCDNCluster();
+
+})(window);
