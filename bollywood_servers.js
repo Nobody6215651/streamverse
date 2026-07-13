5db1d2017dbaf5c842260d0cd7dc2ef57a719b9f
Nobody6215651 <saimmirza652@gmail.com>
2026-07-13 05:33:43 -0700

    Update bollywood_servers.js

--- bollywood_servers.js
+++ bollywood_servers.js
@@ -1,29 +1,62 @@
 /**
- * StreamVerse Dedicated Bollywood & Desi Distribution Streams Array
+ * StreamVerse Bollywood and Regional Asian CDN Distribution Matrix
+ * Path: /js/bollywood_servers.js
  */
-const bollywoodServersList = [
-    {
-        name: "Desi Master Node (VidSrc.cc)",
-        generateUrl: (tmdbId, s, e, type) => {
-            return type === 'movie'
-                ? `https://vidsrc.cc/vidsrc/movie/${tmdbId}`
-                : `https://vidsrc.cc/vidsrc/tv/${tmdbId}/${s}/${e}`;
+(function (window) {
+    'use strict';
+
+    class BollywoodCDNCluster {
+        constructor() {
+            this.clusterRegion = "SOUTH-ASIA-MUMBAI";
+            this.routingNodes = [
+                {
+                    id: "b-node-1",
+                    name: "Regional CDN Edge (VidSrc.cc)",
+                    priority: 10,
+                    generateUrl: (tmdbId, s, e, type) => {
+                        const secureId = encodeURIComponent(tmdbId);
+                        return type === 'movie'
+                            ? `https://vidsrc.cc/vidsrc/movie/${secureId}`
+                            : `https://vidsrc.cc/vidsrc/tv/${secureId}/${Number(s)}/${Number(e)}`;
+                    }
+                },
+                {
+                    id: "b-node-2",
+                    name: "Secondary Regional Node (VidSrc.xyz)",
+                    priority: 7,
+                    generateUrl: (tmdbId, s, e, type) => {
+                        const secureId = encodeURIComponent(tmdbId);
+                        return type === 'movie'
+                            ? `https://vidsrc.xyz/embed/movie/${secureId}`
+                            : `https://vidsrc.xyz/embed/tv/${secureId}/${Number(s)}/${Number(e)}`;
+                    }
+                },
+                {
+                    id: "b-node-3",
+                    name: "Fallback Mirror Node (SuperEmbed)",
+                    priority: 4,
+                    generateUrl: (tmdbId, s, e, type) => {
+                        const secureId = encodeURIComponent(tmdbId);
+                        return type === 'movie'
+                            ? `https://multiembed.mov/?video_id=${secureId}&tmdb=1`
+                            : `https://multiembed.mov/?video_id=${secureId}&tmdb=1&s=${Number(s)}&e=${Number(e)}`;
+                    }
+                }
+            ];
+            Object.freeze(this.routingNodes);
         }
-    },
-    {
-        name: "Premium Bollywood Server (VidSrc.xyz)",
-        generateUrl: (tmdbId, s, e, type) => {
-            return type === 'movie'
-                ? `https://vidsrc.xyz/embed/movie/${tmdbId}`
-                : `https://vidsrc.xyz/embed/tv/${tmdbId}/${s}/${e}`;
+
+        getNodes() {
+            return this.routingNodes;
         }
-    },
-    {
-        name: "Super-Fast Mirror (SuperEmbed)",
-        generateUrl: (tmdbId, s, e, type) => {
-            return type === 'movie'
-                ? `https://multiembed.mov/?video_id=${tmdbId}&tmdb=1`
-                : `https://multiembed.mov/?video_id=${tmdbId}&tmdb=1&s=${s}&e=${e}`;
+
+        getHighestPriorityNode() {
+            return this.routingNodes[0];
         }
     }
-];
+
+    Object.freeze(BollywoodCDNCluster);
+    Object.freeze(BollywoodCDNCluster.prototype);
+    window.BollywoodServers = new BollywoodCDNCluster();
+
+})(window);
